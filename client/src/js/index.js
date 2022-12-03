2386158126b0b259cc2c7d4a80ab9ac49dcdc829
vpratnicki <vanessa@pratnicki.net>
2022-12-03 14:24:08 -0500

    Chore: Configure the app to use the IndexedDB API

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -6,10 +6,12 @@ import Dog from '../images/dog.png';
 import "../css/index.css";
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initdb } from './database';
 
 
 window.addEventListener('load', function () {
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
+  initdb();
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
   });
